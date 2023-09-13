f780b4c25179173ecd9977caf30a9e33ee6e4dc8
DenAsylbaev <denis.asylbaev@gmail.com>
2023-09-14 00:48:45 +0300

    homework_3

--- app/Http/Controllers/WelcomePageController.php
+++ app/Http/Controllers/WelcomePageController.php
@@ -5,21 +5,9 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-// use App\Http\Controllers\NewsTrait;
 
 class WelcomePageController extends Controller
 {
-    // use NewsTrait;
-    //
-    // public function index()
-    // {
-    //     // dd();
-    //     // return $this->getNews();
-    //     return \view('news.index', [
-    //         'news' => $this->getNews(),
-    //     ]);
-    // }
-
     public function show()
     {
         return "WELCOME TO OUR NEWSPORTAL";
